7eff23b815da9fee2e048d1b0a6ca98f39a04cb0
ConnorGDevelopment <connorgdevelopment@gmail.com>
2020-09-21 14:48:04 -0400

    Initial

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app dark style="height: 100vh">
+    <v-main style="height: 100%">
+      <v-container fluid class="pa-0" style="height: 100%">
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row class="align-center">
+    <v-col cols="2" class="pa-0 text-center">
+      <v-btn icon height="10em" width="10em">
+        <v-icon color="white" size="8em">
+          mdi-gamepad-variant
+        </v-icon>
+      </v-btn>
+    </v-col>
+    <v-col cols="8" style="height: 100%" class="pa-0">
+      <v-card style="height: 80%; margin-top: 10%; border: solid 4px white" tile>
+        <v-card-title style="height: 100%">
+          <v-img :src="require('~/assets/img/logo.jpg')" height="100%" width="100%" />
+        </v-card-title>
+      </v-card>
+      <v-card class="transparent" style="height: 10%">
+        <v-card-title class="text-h2 justify-center">
+          Chubby Chef Gaming
+        </v-card-title>
+      </v-card>
+    </v-col>
+    <v-col cols="2" class="pa-0 text-center">
+      <v-btn icon height="10em" width="10em">
+        <v-icon color="white" size="8em">
+          mdi-chef-hat
+        </v-icon>
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
